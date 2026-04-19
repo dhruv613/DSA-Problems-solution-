129babf93131ade92c482bffc178a0f5e208f58d
DHRUV PRAJAPATI <159617096+dhruv613@users.noreply.github.com>
2026-04-19 16:01:00 +0530

    new method to use in interviews

--- sorting/Color_sorting.py
+++ sorting/Color_sorting.py
@@ -20,4 +20,28 @@ def sortColors(nums):
 
 arr = [2,0,2,1,1,0]
 
-print("Sorted array is:", sortColors(arr))
+print("Sorted-1 array is:", sortColors(arr))
+
+
+# Sorting with conditions and while loop 
+
+def sortColors(nums):
+    left = 0
+    right = len(nums) - 1
+    i = 0
+
+    while i <= right:
+        if nums[i] == 0:
+            nums[left], nums[i] = nums[i], nums[left]
+            left += 1
+            i += 1
+        elif nums[i] == 2:
+            nums[right], nums[i] = nums[i], nums[right]
+            right -= 1
+        else:
+            i += 1
+
+    return nums
+
+arr = [2,0,2,1,1,0]
+print("Sorted-2 array is:", sortColors(arr))
