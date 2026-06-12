66f5f7503c95fd458e45917e6713e5b26bb0ba85
dhruv613 <dp334218@gmail.com>
2026-06-12 14:14:38 +0530

    ff

--- Linked_List/delete_n_Node.py
+++ Linked_List/delete_n_Node.py
@@ -1,27 +1,76 @@
-# Definition for singly-linked list.
-from platform import node
 
 
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+import math
+import os
+import random
+import re
+import sys
+import heapq
 
-class Solution:
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
+#
+# Complete the 'shortestReach' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY edges
+#  3. INTEGER s
+#
 
-# The problem is to delete a node in a singly linked list, given only access to that node.
-# The solution is to copy the value of the next node to the current node, and then delete the next node by changing the next pointer of the current node to skip the next node. This effectively removes the next node from the linked list, while keeping the current node in place with its new value.
-node = ListNode(4)
-example = ListNode(4)
-example.next = ListNode(5)
-example.next.next = ListNode(1)
-example.next.next.next = ListNode(9)
-Solution().deleteNode(example.next)
-print(example.next.val) # Output: 1
+def shortestReach(n, edges, s):
+    adj_list = [[] for _ in range(n)]
+    for edge in edges:
+        u, v, w = edge
+        adj_list[u-1].append((v-1, w))
+        adj_list[v-1].append((u-1, w))
+
+    distances = [float('inf')] * n
+    distances[s-1] = 0
+
+    min_heap = [(0, s-1)]
+    while len(min_heap) != 0:
+        current_distance, current_node = heapq.heappop(min_heap)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in adj_list[current_node]:
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(min_heap, (distance, neighbor))
+
+    result = []
+    for i, distance in enumerate(distances):
+        if i == s - 1:
+            continue
+        result.append(-1 if distance == float('inf') else distance)
+
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        edges = []
+
+        for _ in range(m):
+            edges.append(list(map(int, input().rstrip().split())))
+
+        s = int(input().strip())
+
+        result = shortestReach(n, edges, s)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
